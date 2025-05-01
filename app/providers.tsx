80ab9269dcf80fd75ffbce0e3afb7de08d451a92
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-01 20:42:22 +0400

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,17 +3,13 @@
 import { WagmiProvider } from 'wagmi'
 import { RainbowKitProvider, lightTheme } from '@rainbow-me/rainbowkit'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient, config } from '@/lib/wagmiConfig'
+import { queryClient, config } from '../lib/wagmiConfig'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider
-          modalSize="compact"
-          theme={lightTheme()}
-          locale="en-US"
-        >
+        <RainbowKitProvider modalSize="compact" theme={lightTheme()} locale="en-US">
           {children}
         </RainbowKitProvider>
       </QueryClientProvider>
