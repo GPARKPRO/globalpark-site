d78ae7bd5d6a515653924030f00903c2fbb85122
GlobalPark-DAO <mxm0010701@gmail.com>
2025-05-01 16:33:03 +0400

    Update ConnectWallet.tsx

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -1,23 +1,30 @@
-'use client'
-
+import { useEffect } from 'react'
+import { useAccount } from 'wagmi'
+import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
 
-// upd
 const ConnectButton = dynamic(
-  () => import('@rainbow-me/rainbowkit').then((mod) => mod.ConnectButton),
+  () => import('@rainbow-me/rainbowkit').then(mod => mod.ConnectButton),
   { ssr: false }
 )
 
 export default function ConnectWallet() {
+  const { isConnected } = useAccount()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (isConnected) {
+      router.push('/dashboard')
+    }
+  }, [isConnected, router])
+
   return (
-    <div className="mt-8">
-      <div className="inline-block border border-white px-5 py-2 font-medium rounded hover:bg-white hover:text-black transition text-white">
-        <ConnectButton
-          chainStatus="icon"
-          showBalance={false}
-          accountStatus="address"
-        />
-      </div>
+    <div className="mt-6">
+      <ConnectButton
+        accountStatus={{ smallScreen: 'avatar', largeScreen: 'full' }}
+        chainStatus="icon"
+        showBalance={false}
+      />
     </div>
   )
 }
