2057403e0656ec8c2e412895eaafdb32ad7b4845
GlobalPark-DAO <mxm0010701@gmail.com>
2025-05-01 17:14:53 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,37 @@
 // app/layout.tsx
 
-import '@/styles/globals.css'
-import type { Metadata } from 'next'
+import './globals.css'
+import { ReactNode } from 'react'
+import { config, queryClient } from '@/lib/wagmiConfig'
+import { WagmiProvider } from 'wagmi'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { RainbowKitProvider, lightTheme } from '@rainbow-me/rainbowkit'
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Global Park',
   description: 'A Decentralized Initiative for Art, Technology & Collective Memory',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">{children}</body>
+      <body className="bg-black text-white">
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+            <RainbowKitProvider
+              locale="en-US"
+              theme={lightTheme({
+                accentColor: '#000000',
+                accentColorForeground: '#ffffff',
+                borderRadius: 'medium',
+                fontStack: 'system',
+              })}
+            >
+              {children}
+            </RainbowKitProvider>
+          </QueryClientProvider>
+        </WagmiProvider>
+      </body>
     </html>
   )
 }
