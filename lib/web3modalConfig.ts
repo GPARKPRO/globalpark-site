c67328e20491b0d1948688db4520fdb5d36e4c4b
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-01 23:54:46 +0400

    Update web3modalConfig.ts

--- lib/web3modalConfig.ts
+++ lib/web3modalConfig.ts
@@ -1,42 +1,29 @@
 'use client'
 
-import { EthereumClient, w3mProvider } from '@web3modal/ethereum'
-import { configureChains, createConfig } from '@wagmi/core'
-import { mainnet } from '@wagmi/core/chains'
-import { Web3Modal } from '@web3modal/html'
-import { InjectedConnector } from '@wagmi/core/connectors/injected'
-import { WalletConnectConnector } from '@wagmi/core/connectors/walletConnect'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { mainnet } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
 
 const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!
 
-// Configure chains & providers
 const { chains, publicClient } = configureChains(
   [mainnet],
-  [w3mProvider({ projectId })]
+  [w3mProvider({ projectId }), publicProvider()]
 )
 
-// Create wagmi config
 export const wagmiConfig = createConfig({
   autoConnect: true,
-  connectors: [
-    new InjectedConnector({ chains }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        projectId,
-        showQrModal: false,
-      },
-    }),
-  ],
-  publicClient,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient
 })
 
-// Create EthereumClient & Web3Modal
 export const ethereumClient = new EthereumClient(wagmiConfig, chains)
-
-export const web3Modal = new Web3Modal({
-  projectId,
-  themeMode: 'dark',
-  themeColor: 'blackWhite',
-  themeBackground: 'themeColor',
-}, ethereumClient)
+export const web3Modal = (
+  <Web3Modal
+    projectId={projectId}
+    ethereumClient={ethereumClient}
+    themeMode="dark"
+  />
+)
