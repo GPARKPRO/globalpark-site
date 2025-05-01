2b0b42cdae33fb7b99c50f3353c8d332ce83c562
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-02 00:42:04 +0400

    Update web3modalConfig.ts

--- lib/web3modalConfig.ts
+++ lib/web3modalConfig.ts
@@ -1,32 +1,20 @@
 'use client'
 
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/react'
-import { configureChains, createConfig } from '@wagmi/core'
-import { mainnet } from '@wagmi/core/chains'
-import { QueryClient } from '@tanstack/react-query'
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+import { mainnet } from 'wagmi/chains'
+import { createConfig } from 'wagmi'
 
-const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!
 
-const { chains, publicClient } = configureChains(
-  [mainnet],
-  [w3mProvider({ projectId })]
-)
-
-export const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: w3mConnectors({ projectId, chains }),
-  publicClient
-})
-
-export const ethereumClient = new EthereumClient(wagmiConfig, chains)
-
-export const queryClient = new QueryClient()
-
-export const web3Modal = (
-  <Web3Modal
-    projectId={projectId}
-    ethereumClient={ethereumClient}
-    themeMode="dark"
-  />
+export const wagmiConfig = createConfig(
+  defaultWagmiConfig({
+    chains: [mainnet],
+    projectId,
+    metadata: {
+      name: 'Global Park',
+      description: 'A Decentralized Initiative for Art, Technology & Collective Memory',
+      url: 'https://globalpark.io',
+      icons: ['https://globalpark.io/logo.png']
+    }
+  })
 )
