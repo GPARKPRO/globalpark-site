acbde66f2481f1c2e6fb307a542d45b1c72a70b0
GlobalPark-DAO <mxm0010701@gmail.com>
2025-05-01 14:24:02 +0400

    Update ConnectWallet.tsx

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -2,6 +2,7 @@
 
 import dynamic from 'next/dynamic'
 
+// Динамически подключаем обычную кнопку
 const ConnectButton = dynamic(
   () => import('@rainbow-me/rainbowkit').then((mod) => mod.ConnectButton),
   { ssr: false }
@@ -10,22 +11,13 @@ const ConnectButton = dynamic(
 export default function ConnectWallet() {
   return (
     <div className="mt-8">
-      <ConnectButton.Custom>
-        {({ account, chain, openConnectModal, mounted }) => {
-          const ready = mounted;
-          const connected = ready && account && chain;
-
-          return (
-            <button
-              onClick={openConnectModal}
-              className="border border-white text-white px-5 py-2 font-medium rounded hover:bg-white hover:text-black transition"
-              type="button"
-            >
-              {connected ? `${account.displayName}` : 'Connect Wallet'}
-            </button>
-          )
-        }}
-      </ConnectButton.Custom>
+      <div className="inline-block border border-white px-5 py-2 font-medium rounded hover:bg-white hover:text-black transition text-white">
+        <ConnectButton
+          chainStatus="icon"
+          showBalance={false}
+          accountStatus="address"
+        />
+      </div>
     </div>
   )
 }
