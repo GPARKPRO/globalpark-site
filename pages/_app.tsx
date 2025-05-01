914b5632605518c68b92b7aced14a3c7e326a560
GlobalPark-DAO <mxm0010701@gmail.com>
2025-05-01 12:45:52 +0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { WagmiConfig, configureChains, createConfig } from 'wagmi'
-import { mainnet } from 'wagmi/chains'
+import { WagmiConfig, createClient, chain, configureChains } from 'wagmi'
 import { publicProvider } from 'wagmi/providers/public'
 import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit'
 import '@rainbow-me/rainbowkit/styles.css'
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet],
+const { chains, provider, webSocketProvider } = configureChains(
+  [chain.mainnet],
   [publicProvider()]
 )
 
 const { connectors } = getDefaultWallets({
   appName: 'Global Park',
-  projectId: 'YOUR_PROJECT_ID', // можешь временно написать "gpark-dev"
   chains
 })
 
-const wagmiConfig = createConfig({
+const client = createClient({
   autoConnect: true,
   connectors,
-  publicClient,
-  webSocketPublicClient,
+  provider,
+  webSocketProvider
 })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig config={wagmiConfig}>
+    <WagmiConfig client={client}>
       <RainbowKitProvider chains={chains}>
         <Component {...pageProps} />
       </RainbowKitProvider>
