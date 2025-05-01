f60d26142e9006f1d9756f9220c2db10b69d7890
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-01 22:51:25 +0400

    Update wagmiConfig.ts

--- lib/wagmiConfig.ts
+++ lib/wagmiConfig.ts
@@ -1,25 +1,31 @@
 'use client'
 
-import { getDefaultConfig } from '@rainbow-me/rainbowkit'
+import { configureChains, createConfig } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
-import { http } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
 import { QueryClient } from '@tanstack/react-query'
+import { walletConnectProvider } from '@rainbow-me/rainbowkit/providers/walletConnect'
 
-export const config = getDefaultConfig({
-  appName: 'Global Park',
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-  chains: [mainnet],
-  transports: {
-    [mainnet.id]: http(
-      `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
-    ),
-  },
-  ssr: true,
-  metadata: {
-    name: 'Global Park',
-    description: 'A Decentralized Initiative for Art, Tech & Memory',
-    url: 'https://globalpark.io',
-  },
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet],
+  [
+    walletConnectProvider({
+      projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+      metadata: {
+        name: 'Global Park',
+        description: 'A Decentralized Initiative for Art, Tech & Memory',
+        url: 'https://globalpark.io',
+      },
+    }),
+    publicProvider(),
+  ]
+)
+
+export const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+  connectors: [],
 })
 
 export const queryClient = new QueryClient()
