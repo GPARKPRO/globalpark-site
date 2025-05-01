abde0155c9e0368c4ed930fc3e37d322adf2ab6f
GlobalPark-DAO <mxm0010701@gmail.com>
2025-05-01 15:52:15 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// next.config.js
 
-module.exports = nextConfig
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value:
+      "default-src 'self'; script-src 'self' 'unsafe-inline' *.vercel.app *.alchemy.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; connect-src *; font-src 'self';",
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+];
+
+module.exports = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
