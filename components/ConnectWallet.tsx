ba9071258dcfbe7ff8a70a5673455fb811e140d2
GlobalPark-DAO <mxm0010701@gmail.com>
2025-05-01 12:57:29 +0400

    Update ConnectWallet.tsx

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -1,15 +1,19 @@
 'use client'
 
-import { ConnectButton } from '@rainbow-me/rainbowkit'
+import dynamic from 'next/dynamic'
+
+// импортируем ConnectButton динамически, только в браузере
+const ConnectButton = dynamic(
+  () =>
+    import('@rainbow-me/rainbowkit').then((mod) => mod.ConnectButton),
+  { ssr: false }
+)
 
 export default function ConnectWallet() {
   return (
     <div className="mt-6">
       <ConnectButton
-        accountStatus={{
-          smallScreen: 'avatar',
-          largeScreen: 'full',
-        }}
+        accountStatus={{ smallScreen: 'avatar', largeScreen: 'full' }}
         chainStatus="icon"
         showBalance={false}
       />
