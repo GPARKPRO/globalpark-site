797708a76402c2f10c791bf04755b2facd1005fe
GlobalPark-DAO <mxm0010701@gmail.com>
2025-05-01 10:47:04 +0400

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">Global Park is coming soon...</h1>
+    </div>
+  );
+}
