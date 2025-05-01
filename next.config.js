60837c36e23726b2cbd235905ffaf1fc4417e0dd
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-02 00:16:36 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,37 +1,7 @@
-// next.config.js
-
-const securityHeaders = [
-  {
-    key: 'Content-Security-Policy',
-    value:
-      "default-src 'self'; script-src 'self' 'unsafe-inline' *.vercel.app *.alchemy.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; connect-src *; font-src 'self';",
-  },
-  {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin',
-  },
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY',
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-];
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: securityHeaders,
-      },
-    ];
-  },
-};
+  swcMinify: true,
+}
+
+module.exports = nextConfig
