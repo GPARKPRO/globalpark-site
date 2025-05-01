0d155f4fbfbe2fafc3ff58407514e965fa7cedc9
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-01 23:36:17 +0400

    Update wagmiConfig.ts

--- lib/wagmiConfig.ts
+++ lib/wagmiConfig.ts
@@ -2,14 +2,12 @@
 
 import { createConfig, http } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
-import { InjectedConnector } from 'wagmi/connectors/injected'
+import { injectedConnector } from '@wagmi/connectors/injected'
 import { QueryClient } from '@tanstack/react-query'
 
 export const config = createConfig({
   connectors: [
-    new InjectedConnector({
-      chains: [mainnet],
-    }),
+    injectedConnector({ chains: [mainnet] }),
   ],
   chains: [mainnet],
   transports: {
