177758c88fa389a352e2a3f9130e66259ffe13ba
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-01 19:25:59 +0400

    Update ConnectWallet.tsx

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -1,16 +1,10 @@
 'use client'
 
 import { ConnectButton } from '@rainbow-me/rainbowkit'
-import { useEffect } from 'react'
 
 export default function ConnectWallet() {
-  // Можно добавить отладку, если нужно видеть в консоли, когда компонент монтируется
-  useEffect(() => {
-    console.log('[ConnectWallet] rendered')
-  }, [])
-
   return (
-    <div className="mt-10 z-10 relative w-full max-w-xs sm:max-w-sm md:max-w-md">
+    <div className="z-10 relative">
       <ConnectButton
         showBalance={false}
         accountStatus="address"
