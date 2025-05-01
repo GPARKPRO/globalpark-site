dc299ff45bb702eb0378dfd528888b94a7b9eb16
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-01 20:11:57 +0400

    Update wagmiConfig.ts

--- lib/wagmiConfig.ts
+++ lib/wagmiConfig.ts
@@ -1,21 +1,20 @@
 'use client'
 
-import { createConfig, http } from 'wagmi'
+import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { mainnet } from 'wagmi/chains'
-import { walletConnect } from 'wagmi/connectors'
+import { createConfig, http } from 'wagmi'
 import { QueryClient } from '@tanstack/react-query'
 
-export const config = createConfig({
-  chains: [mainnet],
-  connectors: [
-    walletConnect({
-      projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-      showQrModal: true,
-    }),
-  ],
-  transports: {
-    [mainnet.id]: http(`https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`),
-  },
-})
+// Альтернатива Viem для подключения (используется в wagmi@v2)
+export const config = createConfig(
+  getDefaultConfig({
+    appName: 'Global Park',
+    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+    chains: [mainnet],
+    transports: {
+      [mainnet.id]: http(`https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`)
+    },
+  })
+)
 
 export const queryClient = new QueryClient()
