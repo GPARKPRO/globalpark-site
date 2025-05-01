70323b98e8344bc72ac5ed6f8f28f372e2786a0c
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-01 22:55:29 +0400

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,22 +1,15 @@
 'use client'
 
-import { WagmiProvider, http } from 'wagmi'
+import { WagmiProvider } from 'wagmi'
+import { RainbowKitProvider, lightTheme } from '@rainbow-me/rainbowkit'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { config, queryClient } from '@/lib/wagmiConfig'
-import { mainnet } from 'wagmi/chains'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const { wallets } = getDefaultWallets({
-    appName: 'Global Park',
-    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-    chains: [mainnet],
-  })
-
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider chains={[mainnet]} wallets={wallets}>
+        <RainbowKitProvider modalSize="compact" theme={lightTheme()}>
           {children}
         </RainbowKitProvider>
       </QueryClientProvider>
