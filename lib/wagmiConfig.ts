4ec4aae6f2de409e2aea29168844ca574dd36b7a
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-01 20:04:44 +0400

    Update wagmiConfig.ts

--- lib/wagmiConfig.ts
+++ lib/wagmiConfig.ts
@@ -2,33 +2,20 @@
 
 import { createConfig, http } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
+import { walletConnect } from 'wagmi/connectors'
 import { QueryClient } from '@tanstack/react-query'
-import { walletConnect, injected, metaMask, coinbaseWallet } from 'wagmi/connectors'
-
-const alchemyHttpUrl = `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
 
 export const config = createConfig({
   chains: [mainnet],
-  transports: {
-    [mainnet.id]: http(alchemyHttpUrl),
-  },
   connectors: [
-    injected(),
-    metaMask(),
-    coinbaseWallet({
-      appName: 'Global Park',
-    }),
     walletConnect({
       projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-      metadata: {
-        name: 'Global Park',
-        description: 'Connect to the Global Park DAO',
-        url: 'https://globalpark.io',
-        icons: ['https://globalpark.io/logo.png'],
-      },
+      showQrModal: true,
     }),
   ],
-  ssr: true,
+  transports: {
+    [mainnet.id]: http(`https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`),
+  },
 })
 
 export const queryClient = new QueryClient()
