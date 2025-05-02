9663661f32af902de4dfd0dd3b0e401be198ed0e
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-02 10:45:47 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,11 @@
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './lib/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        black: '#000000',
-        white: '#ffffff',
-        gray: {
-          900: '#111111',
-          800: '#1a1a1a',
-          700: '#2e2e2e',
-          600: '#4b4b4b',
-          400: '#9ca3af',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
