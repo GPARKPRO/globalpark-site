c90ede0819f39b85a076241ddbde175ecebd84c0
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-02 11:24:30 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import '../styles/globals.css'
 import type { Metadata } from 'next'
+import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 export const metadata: Metadata = {
