2cdd633abaa09de4a501133ab50f934463517eb3
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-03 17:11:57 +0400

    Update MdxContent.tsx

--- app/components/mdx/MdxContent.tsx
+++ app/components/mdx/MdxContent.tsx
@@ -6,7 +6,7 @@ import remarkGfm from 'remark-gfm';
 
 export default function MdxContent({ children }: { children: string }) {
   return (
-    <article className="prose lg:prose-lg max-w-none">
+    <article className="prose prose-zinc dark:prose-invert lg:prose-lg max-w-none">
       <ReactMarkdown remarkPlugins={[remarkGfm]}>
         {children}
       </ReactMarkdown>
