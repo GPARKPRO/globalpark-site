6ba7eeaaeda4ea29b61032890a382104a13a3183
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-04 00:28:11 +0400

    Create icons.ts

---
+++ lib/icons.ts
@@ -0,0 +1,14 @@
+// lib/icons.ts
+
+export const availableIcons = [
+  'Scheme',
+  'Scheme-1',
+  'Scheme-2',
+  'Scheme-3',
+  'Scheme-4',
+  'Scheme-5',
+  'Scheme-6',
+  'Scheme-7',
+  'Scheme-8',
+  'Scheme-9',
+];
