44f3dee2d735ef64bc2e6e07c485f9b5c1326e33
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-03 14:38:12 +0400

    Update MdxContent.tsx

--- app/components/mdx/MdxContent.tsx
+++ app/components/mdx/MdxContent.tsx
@@ -2,8 +2,8 @@ import { ReactElement } from 'react';
 
 export default function MdxContent({ children }: { children: ReactElement }) {
   return (
-    <div className="prose space-y-4 prose-lg custom max-w-5xl">
-      {children]
+    <div className="prose space-y-4 prose-lg max-w-5xl">
+      {children}
     </div>
   );
 }
