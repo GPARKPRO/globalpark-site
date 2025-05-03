b0b019865148378dbf3b714e717044545c6eff66
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-04 00:33:51 +0400

    Update MdxContent.tsx

--- app/components/mdx/MdxContent.tsx
+++ app/components/mdx/MdxContent.tsx
@@ -3,11 +3,17 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import Icon from '@/components/Icon';
 
 export default function MdxContent({ children }: { children: string }) {
   return (
-    <article className="prose prose-zinc dark:prose-invert lg:prose-lg max-w-none">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>
+    <article className="prose prose-zinc dark:prose-invert lg:prose-lg max-w-none prose-headings:text-white prose-p:text-white">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          Icon: ({ node, ...props }) => <Icon {...(props as any)} />,
+        }}
+      >
         {children}
       </ReactMarkdown>
     </article>
