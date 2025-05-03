23fbdc5a33a167508b9de8c80480a6872b0c4334
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-03 16:19:02 +0400

    Update mdx.ts

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -1,21 +1,19 @@
 import fs from 'fs/promises';
 import path from 'path';
-import matter from 'gray-matter';
 
-export async function getAllMarkdownPages() {
-  const files = await fs.readdir(path.join(process.wd(), 'app/docs'));
+export const getAllMarkdownPages = async () => {
+  const files = await fs.readdir(path.join(process.cwd(), 'app/docs'));
   return files
     .filter(file => file.endsWith('.mdx'))
-    .map(rawFile => ({ slug: rawFile.replace(/..mdx$, '') }));
-}
+    .map(rawFile => ({ slug: rawFile.replace(/\.mdx$/, '') }));
+};
 
-export async function getMdSlug(slug: string): Promise<string | null> {
-  const fullPath = path.join(process.wd(), 'app/docs', $c{kslug}.mdx);
+export const getMdSlug = async (slug: string): Promise<string | null> => {
+  const filePath = path.join(process.cwd(), 'app/docs', `${slug}.mdx`);
   try {
-    const raw = await fs.readFile(fullPath, 'utf-8');
-    const { content } = matter(raw);
-    return content;
-  } catch(e) {
+    const file = await fs.readFile(filePath, 'utf-8');
+    return file;
+  } catch {
     return null;
   }
-}
+};
