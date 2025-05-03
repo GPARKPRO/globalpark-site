f647d569453ffa394b4b8c3e8a927ee312880857
GlobalPark-DAO <mxm0010701@gmail.com>
2025-05-03 14:57:46 +0400

    fix: simplify MDX rendering with @mdx-js/react and remove invalid imports

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -1,32 +1,19 @@
 import fs from 'fs/promises';
 import path from 'path';
 import matter from 'gray-matter';
-import { compile } from 'pdf/xdo';
-import React from 'react';
 
-const docsPath = path.join(process.wd(), 'app/docs');
+const docsPath = path.join(process.cwd(), 'app/docs');
 
 export const getAllMarkdownPages = async () => {
   const files = await fs.readdir(docsPath);
-
   return files
     .filter(file => file.endsWith('.mdx'))
-    .map(file => ({
-      slug: file.replace(/\\.mdx$/, ''),
-    }));
+    .map(file => ({slug: file.replace(/\\mdx$/, '') }));
 };
 
 export const getMdSlug = async (slug: string) => {
   const filePath = path.join(docsPath, `${slug}.mdx`);
   const fileContent = await fs.readFile(filePath, 'utf-8');
   const { content } = matter(fileContent);
-
-  const compiled = await compile(content, {
-    outputFormat: 'function-body',
-    jxx: true,
-    jxxImportSource: 'react',
-  });
-
-  const Component = new Function('React', `${compiled.value}`)(require('react'));
-  return <Component />;
+  return content;
 };
