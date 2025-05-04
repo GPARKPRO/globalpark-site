16958ea311cafa5096711f557cffa6ad479a3fff
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-05 00:42:13 +0400

    Update page.tsx

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -19,7 +19,6 @@ export default function DocsIndexPage() {
 
   return (
     <main className="max-w-4xl mx-auto px-6 py-16">
-      <main className="relative max-w-4xl mx-auto px-6 py-16">
   <div className="absolute top-8 right-8 opacity-10 w-24 h-24 pointer-events-none">
     <img src="/icons/Scheme-2.svg" alt="" className="w-full h-full" />
   </div>
