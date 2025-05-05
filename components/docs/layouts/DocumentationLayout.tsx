bb3235398335cb0c7ac60ec18b195cc2869e5b4b
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-05 12:17:25 +0400

    Create DocumentationLayout.tsx

---
+++ components/docs/layouts/DocumentationLayout.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  children: React.ReactNode;
+  nav?: React.ReactNode; // можно передавать любую навигацию
+}
+
+export default function DocumentationLayout({ children, nav }: Props) {
+  return (
+    <div className="relative flex w-full max-w-screen-xl mx-auto px-6 lg:px-12 xl:px-20">
+      <aside className="hidden lg:block w-64 shrink-0 pt-32">
+        {nav}
+      </aside>
+      <main className="flex-1 pt-20 pb-32 prose dark:prose-invert max-w-none">
+        {children}
+      </main>
+    </div>
+  );
+}
