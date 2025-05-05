12b2339bb4b1373be042636a0a4e5a8c774ec54c
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-05 14:10:28 +0400

    Update DocumentationLayout.tsx

--- components/docs/layouts/DocumentationLayout.tsx
+++ components/docs/layouts/DocumentationLayout.tsx
@@ -1,15 +1,22 @@
+import MobileDocsNav from '@/components/docs/common/MobileDocsNav';
+
 interface Props {
   children: React.ReactNode;
-  nav?: React.ReactNode; // optional navigation
+  nav?: React.ReactNode;
 }
 
 export default function DocumentationLayout({ children, nav }: Props) {
   return (
     <div className="relative flex flex-col lg:flex-row w-full max-w-screen-xl mx-auto px-6 lg:px-12 xl:px-20">
-      <aside className="block w-full lg:w-64 shrink-0 pt-8 lg:pt-32">
+      {/* Desktop Navigation */}
+      <aside className="hidden lg:block w-64 shrink-0 pt-32">
         {nav}
       </aside>
+
+      {/* Main Content */}
       <main className="flex-1 pt-20 pb-32 prose dark:prose-invert max-w-none">
+        {/* Mobile Navigation */}
+        <MobileDocsNav />
         {children}
       </main>
     </div>
