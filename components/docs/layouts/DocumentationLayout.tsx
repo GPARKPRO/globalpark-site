308426c66b2719b32da7b453c5e7c8f79c4ac337
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-05 14:04:42 +0400

    Update DocumentationLayout.tsx

--- components/docs/layouts/DocumentationLayout.tsx
+++ components/docs/layouts/DocumentationLayout.tsx
@@ -1,12 +1,12 @@
 interface Props {
   children: React.ReactNode;
-  nav?: React.ReactNode; // any navigation here
+  nav?: React.ReactNode; // optional navigation
 }
 
 export default function DocumentationLayout({ children, nav }: Props) {
   return (
-    <div className="relative flex w-full max-w-screen-xl mx-auto px-6 lg:px-12 xl:px-20">
-      <aside className="hidden lg:block w-64 shrink-0 pt-32">
+    <div className="relative flex flex-col lg:flex-row w-full max-w-screen-xl mx-auto px-6 lg:px-12 xl:px-20">
+      <aside className="block w-full lg:w-64 shrink-0 pt-8 lg:pt-32">
         {nav}
       </aside>
       <main className="flex-1 pt-20 pb-32 prose dark:prose-invert max-w-none">
