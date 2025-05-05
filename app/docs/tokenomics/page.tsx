578101ac481ba4462a7d9c353510613eaaafed34
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-05 13:12:27 +0400

    Update page.tsx

--- app/docs/tokenomics/page.tsx
+++ app/docs/tokenomics/page.tsx
@@ -3,6 +3,7 @@ import DocumentationLayout from '@/components/docs/layouts/DocumentationLayout';
 import TokenomicsNav from '@/components/docs/tokenomics/TokenomicsNav';
 
 const Introduction = dynamic(() => import('@/components/docs/tokenomics/Introduction'));
+const TokenUtility = dynamic(() => import('@/components/docs/tokenomics/TokenUtility'));
 
 export default function TokenomicsPage() {
   return (
