62ee6e61a017dc3b95dd4cd84ec7f9fb3d46a5ad
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-06 18:14:18 +0400

    Update metadata.ts

--- app/dao/metadata.ts
+++ app/dao/metadata.ts
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
     url: 'https://globalpark.io/dao',
     images: [
       {
-        url: 'https://globalpark.io/opengraph-v2.png',
+        url: 'https://globalpark.io/OG',
         width: 1200,
         height: 630,
         alt: 'Global Park DAO'
@@ -21,6 +21,6 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: 'Global Park DAO',
     description: 'Own a piece of decentralized legacy. Govern space, culture, and permanence on-chain.',
-    images: ['https://globalpark.io/opengraph-v2.png']
+    images: ['https://globalpark.io/OG.png']
   }
 }
