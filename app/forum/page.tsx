182cca0194ced708a5362ffb6ec31ffaa9975f2b
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-06 16:37:53 +0400

    Update page.tsx

--- app/forum/page.tsx
+++ app/forum/page.tsx
@@ -12,14 +12,8 @@ export default function ForumHomePage() {
           href="/forum/new"
           className="border border-yellow-400 text-yellow-300 px-4 py-2 rounded hover:bg-yellow-400 hover:text-black transition duration-200"
         >
-          ➕ New Topic
+          + New Topic
         </Link>
-      </div>
-
-      <div className="hidden md:grid grid-cols-12 px-2 py-3 text-sm text-gray-400 border-b border-white/10 mb-4">
-        <div className="col-span-7">Topic</div>
-        <div className="col-span-2">Replies</div>
-        <div className="col-span-3 text-right">Last Activity</div>
       </div>
 
       <TopicList />
