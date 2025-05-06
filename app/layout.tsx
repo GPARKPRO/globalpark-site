3a6d0a883ef6a010e00a91fdad51cc36541bccd5
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-06 18:15:41 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               ],
               logo: {
                 type: 'image',
-                url: 'https://globalpark.io/opengraph-v2.png'
+                url: 'https://globalpark.io/OG.png'
               }
             })
           }}
