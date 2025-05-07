5e29e9f190931ab7a1178ce7fbfb3d19250954f9
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-07 13:02:39 +0400

    Update Web3Provider.tsx

--- components/Web3Provider.tsx
+++ components/Web3Provider.tsx
@@ -6,7 +6,7 @@ import { wagmiConfig, chains } from '../lib/wagmi'
 export default function Web3Provider({ children }: { children: React.ReactNode }) {
   return (
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider chains={chains} locale="en">
         {children}
       </RainbowKitProvider>
     </WagmiConfig>
