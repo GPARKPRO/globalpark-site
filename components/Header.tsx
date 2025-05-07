a2b6d1dd7d7540e15569e89de98ad67e14cee094
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-07 12:33:15 +0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -85,7 +85,7 @@ export default function Header() {
       </div>
 
       <div className="flex items-center space-x-4">
-        <ConnectWallet />
+      //  <ConnectWallet />
       </div>
     </header>
   )
