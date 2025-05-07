27c6e460caa35faa4a1cba6e802f84346bea1e46
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-07 20:59:09 +0400

    Update ConnectWallet.tsx

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -4,15 +4,8 @@ import { ConnectButton } from '@rainbow-me/rainbowkit'
 
 export default function ConnectWallet() {
   return (
-    <div className="flex items-center gap-4 mt-2">
-      <ConnectButton
-        accountStatus={{
-          smallScreen: 'avatar',
-          largeScreen: 'full',
-        }}
-        showBalance={false}
-        chainStatus="icon"
-      />
+    <div className="mt-2">
+      <ConnectButton />
     </div>
   )
 }
