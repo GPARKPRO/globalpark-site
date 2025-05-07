9895ff158e2996ed87cc13ac657711042d7d1ecd
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-07 11:25:43 +0400

    Create Web3Provider.tsx

---
+++ components/Web3Provider.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { WagmiConfig } from 'wagmi'
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { wagmiConfig, chains } from '../lib/wagmi'
+
+export default function Web3Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        {children}
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
+}
