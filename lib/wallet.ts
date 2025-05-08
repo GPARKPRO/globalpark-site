4c10f12cf37e83cfad3bffe0a04a2e055549483b
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-09 00:27:36 +0400

    Update wallet.ts

--- lib/wallet.ts
+++ lib/wallet.ts
@@ -1,31 +1,14 @@
-'use client'
-
 import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { http } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
-import { WagmiProvider } from 'wagmi'
-import { RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const config = getDefaultConfig({
+export const chains = [mainnet]
+
+export const wagmiConfig = getDefaultConfig({
   appName: 'GlobalPark',
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
-  chains: [mainnet],
+  chains,
   transports: {
     [mainnet.id]: http()
   }
 })
-
-const queryClient = new QueryClient()
-
-export function Web3Provider({ children }: { children: React.ReactNode }) {
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider theme={darkTheme()}>
-          {children}
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  )
-}
