d43ae5898947e9ad3a9ae9a04cff1b78dfc059ae
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-08 11:18:56 +0400

    Update Web3Provider.tsx

--- components/Web3Provider.tsx
+++ components/Web3Provider.tsx
@@ -1,32 +1,29 @@
 'use client'
 
 import '@rainbow-me/rainbowkit/styles.css'
-import { WagmiProvider, createConfig, http, configureChains } from 'wagmi'
+import { WagmiProvider, createConfig, http } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
 import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { ReactNode } from 'react'
 
-const { chains, publicClient } = configureChains(
-  [mainnet],
-  [http()]
-)
-
 const { connectors } = getDefaultWallets({
-  appName: 'Global Park',
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
-  chains,
+  appName: 'GlobalPark',
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+  chains: [mainnet],
 })
 
 const config = createConfig({
-  autoConnect: true,
+  chains: [mainnet],
   connectors,
-  publicClient,
+  transports: {
+    [mainnet.id]: http(),
+  },
 })
 
 export default function Web3Provider({ children }: { children: ReactNode }) {
   return (
     <WagmiProvider config={config}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider chains={[mainnet]}>
         {children}
       </RainbowKitProvider>
     </WagmiProvider>
