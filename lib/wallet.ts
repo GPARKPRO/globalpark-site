b2dcad439f41e836271256d03cd48bac20629cbd
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-09 00:46:27 +0400

    Update wallet.ts

--- lib/wallet.ts
+++ lib/wallet.ts
@@ -1,6 +1,16 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit'
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { http } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
 
 export const wagmiConfig = getDefaultConfig({
   appName: 'GlobalPark',
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string
-})
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
+  chains: [mainnet, polygon, optimism, arbitrum],
+  transports: {
+    [mainnet.id]: http(),
+    [polygon.id]: http(),
+    [optimism.id]: http(),
+    [arbitrum.id]: http(),
+  },
+  ssr: true,
+});
