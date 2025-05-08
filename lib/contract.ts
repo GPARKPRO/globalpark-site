cc7468dbe21370a69051e5989e6d3635bb3ca9ab
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-09 01:31:46 +0400

    Create contract.ts

---
+++ lib/contract.ts
@@ -0,0 +1,16 @@
+// lib/contract.ts
+
+import { BrowserProvider, Contract } from 'ethers';
+import GPARK_ABI from './GPARKTokenABI.json';
+
+const CONTRACT_ADDRESS = '0xA88C78A9b635c9724103bAA7745c2A32E9b9F1da';
+
+export const getGparkContract = async (): Promise<Contract | null> => {
+  if (typeof window === 'undefined' || !(window as any).ethereum) return null;
+
+  const provider = new BrowserProvider((window as any).ethereum);
+  const signer = await provider.getSigner();
+  const contract = new Contract(CONTRACT_ADDRESS, GPARK_ABI, signer);
+
+  return contract;
+};
