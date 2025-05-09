061ecbbe34c45139ed24fb0793775e2f588fbe42
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-09 13:42:26 +0400

    Update contract.ts

--- lib/contract.ts
+++ lib/contract.ts
@@ -1,12 +1,13 @@
 import { getWalletClient, getAccount } from '@wagmi/core'
 import { getContract } from 'viem'
 import GPARK_ABI from './GPARKTokenABI.json'
+import { wagmiConfig } from './wagmi'
 
 const CONTRACT_ADDRESS = '0xA88C78A9b635c9724103bAA7745c2A32E9b9F1da'
 
 export const getGparkContract = async () => {
-  const { address, chainId } = getAccount()
-  const walletClient = await getWalletClient({ chainId })
+  const { address, chainId } = getAccount(wagmiConfig)
+  const walletClient = await getWalletClient(wagmiConfig, { chainId })
 
   if (!walletClient || !address) {
     throw new Error('Wallet not connected')
