dbcccdfc4e200b7537b469b8bda488efed3c4df4
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-09 13:46:23 +0400

    Update wagmi.ts

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -2,6 +2,7 @@ import { getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { configureChains, createConfig } from 'wagmi'
 import { mainnet, polygon, optimism, arbitrum, base, zora } from 'wagmi/chains'
 import { publicProvider } from '@wagmi/core/providers/public'
+
 const { chains, publicClient } = configureChains(
   [mainnet, polygon, optimism, arbitrum, base, zora],
   [publicProvider()]
