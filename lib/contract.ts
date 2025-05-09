092ef1af99d83e10b65aa13dbe553bfdebf96eec
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-09 12:51:49 +0400

    Update contract.ts

--- lib/contract.ts
+++ lib/contract.ts
@@ -1,16 +1,19 @@
-import { Abi, getContract, GetContractReturnType } from 'viem'
+import { getContract } from 'viem'
 import { getWalletClient } from 'wagmi/actions'
-import abiJson from './GPARKTokenABI.json'
 
-const GPARK_ABI = abiJson as Abi
 const CONTRACT_ADDRESS = '0xA88C78A9b635c9724103bAA7745c2A32E9b9F1da'
 
-type GparkContractType = GetContractReturnType<
-  typeof GPARK_ABI,
-  'wallet'
->
+export const GPARK_ABI = [
+  {
+    name: 'balanceOf',
+    type: 'function',
+    stateMutability: 'view',
+    inputs: [{ name: 'account', type: 'address' }],
+    outputs: [{ name: '', type: 'uint256' }],
+  }
+] as const
 
-export const getGparkContract = async (): Promise<GparkContractType> => {
+export const getGparkContract = async () => {
   const walletClient = await getWalletClient()
   if (!walletClient) throw new Error('Wallet client not available')
 
