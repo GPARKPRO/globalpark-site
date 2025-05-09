47054ec6d35dd82123d619f65bc2d879f352aa31
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-09 12:02:40 +0400

    Create wagmi.ts

---
+++ lib/wagmi.ts
@@ -0,0 +1,24 @@
+import { getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { configureChains, createConfig } from 'wagmi'
+import { mainnet, polygon, optimism, arbitrum, base, zora } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, base, zora],
+  [publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'Global Park DAO',
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+  chains,
+})
+
+export const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+})
+
+export { chains }
