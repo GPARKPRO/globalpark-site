92d961ca92c7a7c424ff1f6b90121142c4cf72ff
GlobalPark-DAO <theglobalpark@gmail.com>
2025-05-09 14:21:13 +0400

    Update Web3Provider.tsx

--- components/Web3Provider.tsx
+++ components/Web3Provider.tsx
@@ -1,24 +1,17 @@
 'use client'
 
 import '@rainbow-me/rainbowkit/styles.css'
-import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { WagmiProvider } from 'wagmi'
-import { mainnet } from 'wagmi/chains'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { ReactNode, useState } from 'react'
+import { wagmiConfig } from '@/lib/wagmiConfig'
 
 export default function Web3Provider({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient())
 
-  const config = getDefaultConfig({
-    appName: 'Global Park',
-    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-    chains: [mainnet],
-    ssr: true,
-  })
-
   return (
-    <WagmiProvider config={config}>
+    <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider locale="en">
           {children}
